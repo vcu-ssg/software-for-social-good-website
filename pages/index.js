f7798beb0f3046587ae6d9fffe948db0b370700c
bigwhippy <69910521+bigwhippy@users.noreply.github.com>
2022-09-28 13:25:20 -0400

    First test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to SSG!
+          Welcome to Software for Social Good!
         </h1>
 
         <p className={styles.description}>
