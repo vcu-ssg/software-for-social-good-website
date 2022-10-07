66dad0066fffc6013cc1958b2e285da90eda7599
Daniel <tdanielhuynh@gmail.com>
2022-10-07 14:44:39 -0400

    Fixed header formatting

--- components/Topsection.js
+++ components/Topsection.js
@@ -5,7 +5,7 @@ const Topsection = () =>
     return(
         <div>
             <div>
-                <h1 className={ styles.main }>Software for Social \nGood</h1>
+                <h1 className={ styles.main }>Software for Social <br/ > Good</h1>
                 <h5 className={ styles.sub }>We connect the students with outer communities through projects that bring social impact.</h5>
                 <button className={ styles.button }> Learn more about us </button>
                 <div className={ styles.emptyBoxOne}>
