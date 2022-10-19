ba83a183b1bf9c65ad9883075396b54bac251188
swetaparajuli <parajulis4@vcu.edu>
2022-10-18 23:31:42 -0400

    made carousel for interested in list

--- components/ForStudentsLanding.js
+++ components/ForStudentsLanding.js
@@ -6,7 +6,53 @@ const ForStudentsLanding = () =>
         <div>
             <div>
                 <h1 className={ styles.main }>I'm interested in:</h1>
-                <h1 className= {styles.scrollWheel}> {interests.map((interest) => { return interest + ",\n"})} {lastInterest} </h1>
+                <div class= {styles.frame}>
+                    <div class={styles.carousel}>
+                        <div class= {styles.change_outer}>
+                           <div class= {styles.change_inner}>
+                            <div class= {styles.element}>
+                                User Experience
+                            </div>
+                            <div class= {styles.element}>
+                                Interaction Design
+                            </div>
+                            <div class= {styles.element}>
+                                Data Science
+                            </div>
+                            <div class= {styles.element}>
+                                Software Development
+                            </div>
+                            <div class= {styles.element}>
+                                Networking
+                            </div>
+                            <div class= {styles.element}>
+                               Cross Functionality
+                            </div>
+                            <div class= {styles.element}>
+                               Visual Design
+                            </div>
+                            <div class= {styles.element}>
+                               User Research
+                            </div>
+                            <div class= {styles.element}>
+                               Career Development
+                            </div>
+                            <div class= {styles.element}>
+                               Project Management
+                            </div>
+                            <div class= {styles.element}>
+                               Community Outreach
+                            </div>
+                            <div class= {styles.element}>
+                               Collaboration
+                            </div>
+                            <div class= {styles.element}>
+                               Product Innovation
+                            </div>
+                           </div>
+                        </div>
+                    </div>
+                </div>
                 <h5 className={ styles.sub }>
                     You are at the right place! Join the Software for Social Good, a Vertically Integrated Project (VIP) 
                     at VCU college of engineering to gain real-life experience from projects that gives to our communities.
@@ -23,3 +69,5 @@ const interests = ["User Experience", "Interaction Design", "Data Science", "Sof
 const lastInterest = "Product Innovation";
 
 export default ForStudentsLanding;
+
+// </div> <h1 className= {styles.scrollWheel}> {interests.map((interest) => { return interest + ",\n"})} {lastInterest} </h1>
