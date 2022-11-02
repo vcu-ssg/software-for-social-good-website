0b248abc5a58070d0387063c45d38c7779d1f93b
bigwhippy <69910521+bigwhippy@users.noreply.github.com>
2022-11-02 11:03:38 -0400

    Fixed Our Work page

--- pages/ourwork.js
+++ pages/ourwork.js
@@ -2,21 +2,39 @@ import Navbar from "../components/Navbar";
 import Pastprojects from "../components/Pastprojects";
 import ProposeButton from "../components/ProposeButton";
 import styles from "../styles/OurWork.module.css"
+import Footer from "../components/Footer";
 
 export default function ourwork()
 {
     return(
         <div>
             <Navbar/>
-            <div className={ styles.textContainer }>
-                <h2 className={ styles.mainTextBox }>We are currently searching for exciting projects!</h2>
-                <div className={ styles.subheaderBox }>
-                    <h5>Challenges that your community experiences. Projects that grows student success. Education, transportation, health, logistics - you name it. Don’t hesitate to start a project with us!</h5>
+            <div className= { styles.container }>
+                <div className= { styles.textContainer }>
+                    <h2 className={ styles.mainTextBox }>We are currently searching for exciting projects!</h2>
+                    <div className={ styles.subheaderBox }>
+                        <h5>Challenges that your community experiences. Projects that grows student success. Education, transportation, health, logistics - you name it. Don’t hesitate to start a project with us!</h5>
+                    </div>
+                    <div className={ styles.buttonBox }>
+                        <ProposeButton/>
+                    </div>
                 </div>
             </div>
-            <div className={ styles.buttonBox }>
-                <ProposeButton/>
-            </div>
+            <Footer/>
+
+
+
+            {/* <div className= { styles.container }>
+                <div className={ styles.textContainer }>
+                    <h2 className={ styles.mainTextBox }>We are currently searching for exciting projects!</h2>
+                    <div className={ styles.subheaderBox }>
+                        <h5>Challenges that your community experiences. Projects that grows student success. Education, transportation, health, logistics - you name it. Don’t hesitate to start a project with us!</h5>
+                    </div>
+                </div>
+                <div className={ styles.buttonBox }>
+                    <ProposeButton/>
+                </div>
+            </div> */}
         </div>
     )
 }
