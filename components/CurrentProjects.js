06f1e60d86f375a75a4ff704f284a3af9a25da43
swetaparajuli <parajulis4@vcu.edu>
2022-11-04 12:32:14 -0400

    aligned to 1920x1080 screen

--- components/CurrentProjects.js
+++ components/CurrentProjects.js
@@ -1,22 +1,23 @@
 import styles from '../styles/CurrentProjects.module.css'
 import ProposeButton from './ProposeButton';
-const CurrentProjects = () => 
-{
-    return(
+const CurrentProjects = () => {
+    return (
         <div>
-        <div className= {styles.projectsHeader}>
-            <h3 className= {styles.header}> Current Projects </h3>
-        </div>
-        <div className={ styles.textContainer }>
-                <h2 className={ styles.mainTextBox }>We are currently searching for exciting projects!</h2>
-                <div className={ styles.subheaderBox }>
+            <div className={styles.projectsHeader}>
+                <h3 className={styles.header}> Current Projects </h3>
+            </div>
+            <div className={styles.textContainer}>
+                <h2 className={styles.mainTextBox}>We are currently searching for exciting projects!</h2>
+                <div className={styles.subheaderBox}>
                     <h5>Challenges that your community experiences. Projects that grows student success. Education, transportation, health, logistics - you name it. Don’t hesitate to start a project with us!</h5>
                 </div>
+                <div className={styles.lineOne} />
+                <div className={styles.lineTwo} />
             </div>
-            <div className={ styles.buttonBox }>
-                <ProposeButton/>
+            <div className={styles.buttonBox}>
+                <ProposeButton />
             </div>
-         </div>
+        </div>
     )
 }
 
