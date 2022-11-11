9f2ecbf42273bdf3e1274d6c2dd489415881cc3a
swetaparajuli <parajulis4@vcu.edu>
2022-11-11 12:03:31 -0500

    for students page standard screen resolution size

--- pages/index.js
+++ pages/index.js
@@ -11,24 +11,23 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 
-export default function Home()
-{
-  return(
-    <div className= {styles.container}>
-      <Navbar/>
-      <div className={ styles.topSectionContainer }>
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.topSectionContainer}>
         <div>
-          <h1 className={ styles.homePageTitle }>Software For<br/>Social<br/>Good</h1>
-          <h2 className={ styles.homePageSubTitle }> We connect VCU students with outer<br/>communities through projects that brings<br/>social impact.</h2>
-          <LearnMoreButton/>
+          <h1 className={styles.homePageTitle}>Software For<br />Social<br />Good</h1>
+          <h2 className={styles.homePageSubTitle}> We connect VCU students with outer<br />communities through projects that brings<br />social impact.</h2>
+          <LearnMoreButton />
         </div>
         <div>
           <Image src="/topSecImg.svg" width="719" height="597" placeholder="empty" />
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-    
+
   )
 }
 
